8eb73d2f4ddb08e6e4092f11c5e1af651c14211e
Petr Broz <petr.broz@autodesk.com>
2022-05-16 14:51:08 +0200

    Updated card styling.

--- src/components/card.js
+++ src/components/card.js
@@ -5,9 +5,9 @@ const Card = ({ title, description, screenshotUrl, liveDemoUrl, sourceUrl, tags 
         <div className="col-md-6 col-lg-4">
             <div className="card h-100">
                 <img src={screenshotUrl} alt={description} className="card-img-top"></img>
-                <div className="card-body">
+                <div className="card-body d-flex flex-column">
                     <h5 className="card-title">{title}</h5>
-                    <div className="row mb-2">
+                    <div className="mb-2">
                         {
                             tags && tags.map(tag => (
                                 <span key={tag} className="badge rounded-pill bg-warning m-1">{tag}</span>
@@ -15,7 +15,7 @@ const Card = ({ title, description, screenshotUrl, liveDemoUrl, sourceUrl, tags 
                         }
                     </div>
                     <p className="card-text">{description}</p>
-                    <div className="d-flex justify-content-between align-items-center">
+                    <div className="d-flex justify-content-between align-items-center mt-auto">
                         <div className="btn-group">
                             <a href={sourceUrl} target="_blank" className="btn btn-sm btn-outline-secondary">Source</a>
                             <a href={liveDemoUrl} target="_blank" className="btn btn-sm btn-outline-secondary">Live Demo</a>
