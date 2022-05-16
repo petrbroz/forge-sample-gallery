82b98272fc254690eafaa398dbef5909f2860510
Petr Broz <petr.broz@autodesk.com>
2022-05-16 12:37:25 +0200

    Added initial Bootstrap UI.

--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,29 @@
 import * as React from 'react';
-import { actions, action } from './card.module.css';
 
-const Card = ({ title, description, screenshotUrl, liveDemoUrl, sourceUrl }) => {
+const Card = ({ title, description, screenshotUrl, liveDemoUrl, sourceUrl, tags }) => {
     return (
-        <>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            {screenshotUrl && <img src={screenshotUrl} alt="screenshot" />}
-            <div className={actions}>
-                {liveDemoUrl && <a href={liveDemoUrl} className={action} target="_blank">Live Demo</a>}
-                {sourceUrl && <a href={sourceUrl} className={action} target="_blank">Source Code</a>}
+        <div className="col-md-6 col-lg-4">
+            <div className="card h-100">
+                <img src={screenshotUrl} alt={description} className="card-img-top"></img>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <div className="row mb-2">
+                        {
+                            tags && tags.map(tag => (
+                                <span key={tag} className="badge rounded-pill bg-warning m-1">{tag}</span>
+                            ))
+                        }
+                    </div>
+                    <p className="card-text">{description}</p>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                            <a href={sourceUrl} target="_blank" className="btn btn-sm btn-outline-secondary">Source</a>
+                            <a href={liveDemoUrl} target="_blank" className="btn btn-sm btn-outline-secondary">Live Demo</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </>
+        </div>
     )
 };
 
